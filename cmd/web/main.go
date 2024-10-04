504e579d11b149662257c9aeccd8122aa0261201
Danang Rifai <danang050402@gmail.com>
2024-10-04 12:50:56 +0700

    update host

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -69,7 +69,7 @@ func main() {
 	log.Println("Routes set up")
 
 	// Jalankan server
-	log.Println("Starting server on port 5000")
+	log.Println("Server running on port 3000")
 	if err := app.Listen(":3000"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
